1f9e192fc6345898c55ccf61fd6500d39f9ce554
Selin Sakarya <selinsakarya@outlook.com>
2024-02-18 01:01:08 +0300

    .

--- GarbageCollection/Program.cs
+++ GarbageCollection/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class Program
+{
+    public static long FinalisedObjects = 0;
+    
+    public static long TotalTime = 0;
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+        for (int i = 0; i < 500000; i++)
+        {
+            WithoutDispose obj = new WithoutDispose();
+            
+            obj.DoWork();
+        }
+
+        Console.WriteLine($"Number of finalised objects: {FinalisedObjects/1000}K");
+        
+        double averageObjectLifeTime = 1.0 * TotalTime / FinalisedObjects;
+        
+        Console.WriteLine($"Average resource lifetime: {averageObjectLifeTime}");
+    }
+}
--- GarbageCollection/WithoutDispose.cs
+++ GarbageCollection/WithoutDispose.cs
@@ -7,6 +7,7 @@ public class WithoutDispose
     public WithoutDispose()
     {
         _stopwatch = new Stopwatch();
+        _stopwatch.Start();
     }
 
     ~WithoutDispose()
